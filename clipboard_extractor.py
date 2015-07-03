2966d0c6840fbc13fa96f45ba7cefc2c86f10cad
Vivek Juneja <vivekjuneja@gmail.com>
2015-07-03 16:45:50 +0900

    First Commit

---
+++ clipboard_extractor.py
@@ -0,0 +1,9 @@
+import subprocess
+
+def getClipboardData():
+ p = subprocess.Popen(['pbpaste'], stdout=subprocess.PIPE)
+ retcode = p.wait()
+ data = p.stdout.read()
+ return data
+
+print getClipboardData()
